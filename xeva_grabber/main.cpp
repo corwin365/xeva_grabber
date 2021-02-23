e45b9d562145eeb1ac8dcdecdc0964edb555f8c3
Corwin Wright <git.hub@corwin.me.uk>
2021-02-23 13:25:56 +0000

    First commit

--- xeva_grabber/main.cpp
+++ xeva_grabber/main.cpp
@@ -2,23 +2,44 @@
 #include "XCamera.h" // Xeneth SDK main header.
 #include "XFilters.h" // Xeneth SDK main header.
 
-int main()
+
+/* function declaration */
+int grab_image(char CalPack[100], char OutFile[100]);
+
+int main() {
+
+    /* local variable definition */
+    char CalPack[100] = "XC-(03-08-2020)-LG20000us_13917.xca";
+    char OutFile[100] = "var5.xpng";
+    int ret;
+
+    /* calling a function to get max value */
+    ret = grab_image(CalPack,OutFile);
+
+
+    return 0;
+}
+
+
+
+
+
+
+int grab_image(char CalPack[100],char OutFile[100])
 {
     // Variables
-    XCHANDLE handle = 0; // Handle to the camera
-    ErrCode errorCode = 0; // Used to store returned errorCodes from the SDK functions.
-    word* frameBuffer = 0; // 16-bit buffer to store the capture frame.
-    dword frameSize = 0; // The size in bytes of the raw image.
+    XCHANDLE handle = 0;   // Handle to the camera
+    ErrCode  errorCode = 0; // Used to store returned errorCodes from the SDK functions.
+    word*    frameBuffer = 0; // 16-bit buffer to store the capture frame.
+    dword    frameSize = 0;   // The size in bytes of the raw image.
 
     // Open a connection to the first detected camera by using connection string cam://0
-    printf("Opening connection to cam://0\n");
     handle = XC_OpenCamera("cam://0");
 
     // When the connection is initialised, ...
     if (XC_IsInitialised(handle))
     {
         // ... start capturing
-        printf("Start capturing.\n");
         if ((errorCode = XC_StartCapture(handle)) != I_OK)
         {
             printf("Could not start capturing, errorCode: %lu\n", errorCode);
@@ -26,15 +47,8 @@ int main()
         else if (XC_IsCapturing(handle)) // When the camera is capturing ...
         {
 
-            /*
-            *  The calibration pack.
-            *  Calibration packs are camera specific.
-            *  Copy the calibration pack that comes with the camera onto your system and modify the packname variable accordingly.
-            */
-            XC_LoadCalibration(handle, "XC-(03-08-2020)-LG20000us_13917.xca", XLC_StartSoftwareCorrection);
-
-            // Load thermography filter
-            int thermalFilterId = XC_FLT_Queue(handle, "Thermography", 0);
+            // Calibration Pack
+            XC_LoadCalibration(handle, CalPack, XLC_StartSoftwareCorrection);
 
             // Determine native framesize.
             frameSize = XC_GetFrameSize(handle);
@@ -43,15 +57,12 @@ int main()
             frameBuffer = new word[frameSize / 2];
 
             // ... grab a frame from the camera.
-
-            for (int cnt = 0; cnt < 100; cnt++) {
-                if ((errorCode = XC_GetFrame(handle, FT_NATIVE, XGF_Blocking, frameBuffer, frameSize)) != I_OK)
-                {
-                    printf("Problem while fetching frame, errorCode %lu", errorCode);
-                }
+            if ((errorCode = XC_GetFrame(handle, FT_NATIVE, XGF_Blocking, frameBuffer, frameSize)) != I_OK)
+            {
+                printf("Problem while fetching frame, errorCode %lu", errorCode);
             }
 
-            errorCode = XC_SaveData(handle, "output.xpng", XSD_SaveThermalInfo | XSD_Force16);
+            errorCode = XC_SaveData(handle, OutFile, XSD_SaveThermalInfo | XSD_Force16);
 
         }
     }
@@ -66,7 +77,6 @@ int main()
     if (XC_IsCapturing(handle))
     {
         // ... stop capturing.
-        printf("Stop capturing.\n");
         if ((errorCode = XC_StopCapture(handle)) != I_OK)
         {
             printf("Could not stop capturing, errorCode: %lu\n", errorCode);
@@ -76,11 +86,9 @@ int main()
     // When the handle to the camera is still initialised ...
     if (XC_IsInitialised(handle))
     {
-        printf("Closing connection to camera.\n");
         XC_CloseCamera(handle);
     }
 
-    printf("Clearing buffers.\n");
     if (frameBuffer != 0)
     {
         delete[] frameBuffer;
